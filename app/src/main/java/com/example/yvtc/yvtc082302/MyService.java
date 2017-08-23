eac5ee891e23f56fa3d666efcf402632983348d1
Shaojung <shaojung@gmail.com>
2017-08-23 15:56:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvtc082302/MyService.java
@@ -0,0 +1,29 @@
+package com.example.yvtc.yvtc082302;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("SERV823", "Service Start");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
