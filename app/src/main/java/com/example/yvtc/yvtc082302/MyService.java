75ae29e99e06b278d15b1640a831149578442583
Shaojung <shaojung@gmail.com>
2017-08-23 16:17:43 +0800

    using Service.

--- app/src/main/java/com/example/yvtc/yvtc082302/MyService.java
+++ app/src/main/java/com/example/yvtc/yvtc082302/MyService.java
@@ -1,5 +1,8 @@
 package com.example.yvtc.yvtc082302;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -7,6 +10,7 @@ import android.support.annotation.IntDef;
 import android.util.Log;
 
 public class MyService extends Service {
+    NotificationManager manager;
     public MyService() {
     }
 
@@ -18,6 +22,27 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("SERV823", "Service Start");
+        int i;
+        for (i=1;i<=10;i++)
+        {
+            try {
+                Thread.sleep(1000);
+                Log.d("SERV823", "Countdown:" + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Intent it = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 1212, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(getApplicationContext());
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("這是提醒")
+                .setContentText("這是提醒的內容")
+                .setContentIntent(pi);
+        manager = (NotificationManager) getSystemService(getApplicationContext().NOTIFICATION_SERVICE);
+        manager.notify(321, builder.build());
+
         return super.onStartCommand(intent, flags, startId);
     }
 
